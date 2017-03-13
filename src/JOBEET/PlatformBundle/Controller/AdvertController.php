ffdd3de27e77dd85ba27255f6717996c64f270ae
Ilias Annhari <ilias.annhari@gmail.com>
2017-03-13 17:01:04 +0100

    twig template config

--- src/JOBEET/PlatformBundle/Controller/AdvertController.php
+++ src/JOBEET/PlatformBundle/Controller/AdvertController.php
@@ -17,7 +17,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller
 {
-	
+
 
 	public function indexAction($page)
 	{
@@ -85,6 +85,22 @@ class AdvertController extends Controller
 		return $this->render('JOBEETPlatformBundle:Advert:delete.html.twig');
 	}
 
+	public function menuAction()
+	{
+    // On fixe en dur une liste ici, bien entendu par la suite
+    // on la récupérera depuis la BDD !
+		$listAdverts = array(
+			array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+			array('id' => 5, 'title' => 'Mission de webmaster'),
+			array('id' => 9, 'title' => 'Offre de stage webdesigner')
+			);
+
+		return $this->render('JOBEETPlatformBundle:Advert:menu.html.twig', array(
+      // Tout l'intérêt est ici : le contrôleur passe
+      // les variables nécessaires au template !
+			'listAdverts' => $listAdverts
+			));
+	}
 
 
 

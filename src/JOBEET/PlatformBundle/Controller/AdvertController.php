048966886e4f15cc73be9b4f32fb659411aa6b86
Ilias Annhari <ilias.annhari@gmail.com>
2017-03-13 15:14:38 +0100

    All routes fixed

--- src/JOBEET/PlatformBundle/Controller/AdvertController.php
+++ src/JOBEET/PlatformBundle/Controller/AdvertController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse; 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 
@@ -16,50 +17,75 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AdvertController extends Controller
 {
-	public function indexAction()
+	
+
+	public function indexAction($page)
 	{
-		$content = $this->get('templating')->render('JOBEETPlatformBundle:Advert:index.html.twig');
+    // On ne sait pas combien de pages il y a
+    // Mais on sait qu'une page doit être supérieure ou égale à 1
+		if ($page < 1) {
+      // On déclenche une exception NotFoundHttpException, cela va afficher
+      // une page d'erreur 404 (qu'on pourra personnaliser plus tard d'ailleurs)
+			throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
+		}
 
-		return new Response($content);	
-	}
+    // Ici, on récupérera la liste des annonces, puis on la passera au template
 
-	public function linkAction()
-	{
-		$content = $this->get('templating')->render('JOBEETPlatformBundle:Advert:index.html.twig');
-
-		return new Response($content);
+    // Mais pour l'instant, on ne fait qu'appeler le template
+		return $this->render('JOBEETPlatformBundle:Advert:index.html.twig');
 	}
 
 	public function viewAction($id)
 	{
-    $content = $this->get('templating')->render('JOBEETPlatformBundle:Advert:view.html.twig',array('id' => $id));
+    // Ici, on récupérera l'annonce correspondante à l'id $id
 
-		return new Response($content);	
-	}
-
-
-	public function viewSlugAction($slug, $year, $format)
-	{
-		return new Response(
-			"On pourrait afficher l'annonce correspondant au
-			slug '".$slug."', créée en ".$year." et au format ".$format."."
-			);
+		return $this->render('JOBEETPlatformBundle:Advert:view.html.twig', array(
+			'id' => $id
+			));
 	}
 
 	public function addAction(Request $request)
 	{
-		$session = $request->getSession();
+    // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
 
-    // Bien sûr, cette méthode devra réellement ajouter l'annonce
+    // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+		if ($request->isMethod('POST')) {
+      // Ici, on s'occupera de la création et de la gestion du formulaire
 
-    // Mais faisons comme si c'était le cas
-		$session->getFlashBag()->add('info', 'Annonce bien enregistrée');
+			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
 
-    // Le « flashBag » est ce qui contient les messages flash dans la session
-    // Il peut bien sûr contenir plusieurs messages :
-		$session->getFlashBag()->add('info', 'Oui oui, elle est bien enregistrée !');
+      // Puis on redirige vers la page de visualisation de cettte annonce
+			return $this->redirectToRoute('jobeet_platform_view', array('id' => 5));
+		}
 
-    // Puis on redirige vers la page de visualisation de cette annonce
-		return $this->redirectToRoute('jobeet_platform_view', array('id' => 5));
+    // Si on n'est pas en POST, alors on affiche le formulaire
+		return $this->render('JOBEETPlatformBundle:Advert:add.html.twig');
 	}
+
+	public function editAction($id, Request $request)
+	{
+    // Ici, on récupérera l'annonce correspondante à $id
+
+    // Même mécanisme que pour l'ajout
+		if ($request->isMethod('POST')) {
+			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+
+			return $this->redirectToRoute('jobeet_platform_view', array('id' => 5));
+		}
+
+		return $this->render('JOBEETPlatformBundle:Advert:edit.html.twig');
+	}
+
+	public function deleteAction($id)
+	{
+    // Ici, on récupérera l'annonce correspondant à $id
+
+    // Ici, on gérera la suppression de l'annonce en question
+
+		return $this->render('JOBEETPlatformBundle:Advert:delete.html.twig');
+	}
+
+
+
+
 }

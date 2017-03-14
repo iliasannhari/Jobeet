299f9fefc2a6d939f984fc1c76e6b1cd4d43c0a7
Ilias Annhari <ilias.annhari@gmail.com>
2017-03-14 11:12:02 +0100

    platform in progress part1

--- src/JOBEET/PlatformBundle/Controller/AdvertController.php
+++ src/JOBEET/PlatformBundle/Controller/AdvertController.php
@@ -21,59 +21,84 @@ class AdvertController extends Controller
 
 	public function indexAction($page)
 	{
-    // On ne sait pas combien de pages il y a
-    // Mais on sait qu'une page doit être supérieure ou égale à 1
-		if ($page < 1) {
-      // On déclenche une exception NotFoundHttpException, cela va afficher
-      // une page d'erreur 404 (qu'on pourra personnaliser plus tard d'ailleurs)
-			throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
-		}
-
-    // Ici, on récupérera la liste des annonces, puis on la passera au template
-
-    // Mais pour l'instant, on ne fait qu'appeler le template
-		return $this->render('JOBEETPlatformBundle:Advert:index.html.twig');
+     // Notre liste d'annonce en dur
+		$listAdverts = array(
+			array(
+				'title'   => 'Recherche développpeur Symfony',
+				'id'      => 1,
+				'author'  => 'Alexandre',
+				'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+				'date'    => new \Datetime()),
+			array(
+				'title'   => 'Mission de webmaster',
+				'id'      => 2,
+				'author'  => 'Hugo',
+				'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+				'date'    => new \Datetime()),
+			array(
+				'title'   => 'Offre de stage webdesigner',
+				'id'      => 3,
+				'author'  => 'Mathieu',
+				'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+				'date'    => new \Datetime())
+			);
+		return $this->render('JOBEETPlatformBundle:Advert:index.html.twig', array(
+			'listAdverts' => $listAdverts
+			));
 	}
 
 	public function viewAction($id)
 	{
     // Ici, on récupérera l'annonce correspondante à l'id $id
+		$advert = array(
+      'title'   => 'Recherche développpeur Symfony2',
+      'id'      => $id,
+      'author'  => 'Alexandre',
+      'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+      'date'    => new \Datetime()
+    );
 
-		return $this->render('JOBEETPlatformBundle:Advert:view.html.twig', array(
-			'id' => $id
-			));
+    return $this->render('JOBEETPlatformBundle:Advert:view.html.twig', array(
+      'advert' => $advert
+    ));
+		
 	}
 
-	public function addAction(Request $request)
+	public function addAction( Request $request)
 	{
-    // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
+    
 
-    // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
-		if ($request->isMethod('POST')) {
-      // Ici, on s'occupera de la création et de la gestion du formulaire
 
-			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
 
-      // Puis on redirige vers la page de visualisation de cettte annonce
-			return $this->redirectToRoute('jobeet_platform_view', array('id' => 5));
-		}
+		$advert = array(
+      'title'   => 'Recherche développpeur Symfony',
+      'id'      => 0,
+      'author'  => 'Alexandre',
+      'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+      'date'    => new \Datetime()
+    );
 
-    // Si on n'est pas en POST, alors on affiche le formulaire
-		return $this->render('JOBEETPlatformBundle:Advert:add.html.twig');
+    return $this->render('JOBEETPlatformBundle:Advert:add.html.twig', array(
+      'advert' => $advert
+    ));
 	}
 
 	public function editAction($id, Request $request)
 	{
-    // Ici, on récupérera l'annonce correspondante à $id
 
-    // Même mécanisme que pour l'ajout
-		if ($request->isMethod('POST')) {
-			$request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+		 $advert = array(
+      'title'   => 'Recherche développpeur Symfony',
+      'id'      => $id,
+      'author'  => 'Alexandre',
+      'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+      'date'    => new \Datetime()
+    );
 
-			return $this->redirectToRoute('jobeet_platform_view', array('id' => 5));
-		}
+    return $this->render('JOBEETPlatformBundle:Advert:edit.html.twig', array(
+      'advert' => $advert
+    ));
 
-		return $this->render('JOBEETPlatformBundle:Advert:edit.html.twig');
+    
 	}
 
 	public function deleteAction($id)
@@ -85,7 +110,7 @@ class AdvertController extends Controller
 		return $this->render('JOBEETPlatformBundle:Advert:delete.html.twig');
 	}
 
-	public function menuAction()
+	public function menuAction($limit)
 	{
     // On fixe en dur une liste ici, bien entendu par la suite
     // on la récupérera depuis la BDD !

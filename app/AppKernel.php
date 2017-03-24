b3a164c61f0c8ce581e7b2012bfeb0f48ffc497e
Ilias Annhari <ilias.annhari@gmail.com>
2017-03-24 16:04:36 +0100

    security and manage user DONE

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new JOBEET\PlatformBundle\JOBEETPlatformBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new JOBEET\UserBundle\JOBEETUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- src/JOBEET/UserBundle/DataFixtures/ORM/LoadUser.php
+++ src/JOBEET/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -12,7 +12,7 @@ class LoadUser implements FixtureInterface
   public function load(ObjectManager $manager)
   {
     // Les noms d'utilisateurs à créer
-    $listNames = array('Alexandre', 'Marine', 'Anna');
+    $listNames = array('alex@gmail.com', 'marine@gmail.com', 'anna@gmail.com','ilias.annhari@gmail.com');
 
     foreach ($listNames as $name) {
       // On crée l'utilisateur
@@ -20,16 +20,28 @@ class LoadUser implements FixtureInterface
 
       // Le nom d'utilisateur et le mot de passe sont identiques pour l'instant
       $user->setUsername($name);
-      $user->setPassword($name);
+      $user->setPassword('password');
+      $user->setEmail($name);
+      $user->setEnabled(true);
+
 
       // On ne se sert pas du sel pour l'instant
       $user->setSalt('');
       // On définit uniquement le role ROLE_USER qui est le role de base
-      $user->setRoles(array('ROLE_USER'));
+      $user->setRoles(array('ROLE_MODERATEUR'));
 
       // On le persiste
       $manager->persist($user);
     }
+
+    $super_user = new User;
+    $super_user->setUsername('david@gmail.com');
+    $super_user->setPassword('password');
+    $super_user->setEmail('david@gmail.com');
+    $super_user->setEnabled(true);
+    $super_user->setSalt('');
+    $super_user->setRoles(array('ROLE_AUTEUR'));
+    $manager->persist($super_user);
 
     // On déclenche l'enregistrement
     $manager->flush();

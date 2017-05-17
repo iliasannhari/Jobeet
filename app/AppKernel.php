2870bed4a427f08b9056695dd3b2c3cbce7d8cd7
Ilias Annhari <ilias.annhari@gmail.com>
2017-05-17 14:06:05 +0200

    add sonata admin and config everything

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,14 @@ class AppKernel extends Kernel
             new JOBEET\UserBundle\JOBEETUserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            // These are the other bundles the SonataAdminBundle relies on
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+
+            // And finally, the storage and SonataAdminBundle
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/JOBEET/PlatformBundle/Controller/CategoryAdminController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace JOBEET\PlatformBundle\Controller;
+ 
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+ 
+class CategoryAdminController extends Controller
+{
+ 
+}
